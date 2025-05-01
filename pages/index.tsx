51be111a12b55e62ba608e1a732c9f49989fd37e
marin13c <andrio13marin@outlook.es>
2025-04-30 21:24:02 -0600

    Cambio de la ruta del archivo json

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
 
   useEffect(() => {
     // Cargar el JSON desde la carpeta public
-    fetch("/pokemons.json")  // Asegúrate de que la ruta sea correcta
+    fetch("https://marin13c.github.io/pokedextcg/pokemons.json")  // Asegúrate de que la ruta sea correcta
       .then((res) => res.json())
       .then((data) => setPokemons(data))
       .catch((err) => console.error("Error al cargar los Pokémon", err));

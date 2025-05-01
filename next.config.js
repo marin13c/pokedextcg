53fc60fcc4ead0a83eb41d3883ffd59d97f4af5b
marin13c <andrio13marin@outlook.es>
2025-04-30 21:04:49 -0600

    cambios en next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /**
  * @type {import('next').NextConfig}
  */
+
+const isPdrod = process.env.NODE_ENV = 'production'
+
 const nextConfig = {
+  basePath: isPdrod ? '/pokedextcg': '', 
   output: 'export',
   distDir: 'dist',
 }
